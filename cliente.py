81b7569b0117d22db082cdaf523cd8ff44936630
Adenilson Silva <adenilsonsantos8891@gmail.com>
2024-07-19 18:06:02 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -14,32 +14,39 @@ ESTILO = "utf-8"
 
 def tratar_solicitacao(args):
     """
-    Envia a solicitação para o servidor que está ouvindo em host, porta
+    Envia a solicitação para o servidor que está ouvindo em host e porta,
     e imprime caso haja uma resposta do servidor.
+    
+    Args:
+        args (Namespace): Argumentos contendo host, port e request.
     """
     cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        cliente_socket.connect((args.host, int(args.port)))
-        cliente_socket.sendall(args.request.encode(ESTILO))
-        dados = cliente_socket.recv(TAMANHO_MAX_DADOS)
+        cliente_socket.connect((args.host, int(args.port)))  # Conecta ao servidor especificado
+        cliente_socket.sendall(args.request.encode(ESTILO))  # Envia a solicitação codificada
+        dados = cliente_socket.recv(TAMANHO_MAX_DADOS)  # Recebe a resposta do servidor
         if dados:
-            print(dados.decode(ESTILO))
+            print(dados.decode(ESTILO))  # Imprime a resposta decodificada
     except ConnectionRefusedError:
         print(f"Conexão recusada. O servidor pode estar fora do ar ou a porta {args.port} está incorreta.")
     except Exception as e:
         print(f"Ocorreu um erro inesperado: {e}")
     finally:
-        cliente_socket.close()
+        cliente_socket.close()  # Fecha o socket
 
 def main():
+    """
+    Função principal que parseia os argumentos e chama a função para tratar a solicitação.
+    """
     parser = argparse.ArgumentParser(description="Um script cliente para se comunicar com um servidor genérico")
     
-    parser.add_argument("host")
-    parser.add_argument("port")
-    parser.add_argument("request") # Solicitação deve estar entre aspas
+    parser.add_argument("host")  # Argumento para o endereço do host
+    parser.add_argument("port")  # Argumento para a porta do servidor
+    parser.add_argument("request")  # Solicitação a ser enviada, deve estar entre aspas
     args = parser.parse_args()
 
     tratar_solicitacao(args)
 
 if __name__ == '__main__':
     main()
+
